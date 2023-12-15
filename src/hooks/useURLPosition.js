550621683a5d29f2faa021503dae0e7ccdef9bc8
Anton Osypchuk <osypchuk.ua@gmail.com>
2023-12-15 18:28:24 +0200

    Func for getting lat and lng from url

---
+++ src/hooks/useURLPosition.js
@@ -0,0 +1,9 @@
+import {useSearchParams} from "next/navigation";
+
+export function useUrlPosition() {
+  const [searchParams] = useSearchParams();
+  const lat = Number(searchParams.get("lat"));
+  const lng = Number(searchParams.get("lng"));
+
+  return [lat, lng];
+}
