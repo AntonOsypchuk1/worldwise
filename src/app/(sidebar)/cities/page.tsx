2cc0630d7d802e7d4b479e3f166f501264cce848
Anton Osypchuk <osypchuk.ua@gmail.com>
2023-12-16 12:17:45 +0200

    Routes for cities and countries

---
+++ src/app/(sidebar)/cities/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CityList from "@/components/city/city-list/CityList";
+
+const Page = () => {
+  return (
+    <div>
+      <CityList/>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/app/(sidebar)/countries/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div>
+      countries
+    </div>
+  );
+};
+
+export default Page;
