fba78c83a7d5ea14f4e7f70f9f2f0de04e1d6ea5
Anton Osypchuk <osypchuk.ua@gmail.com>
2023-12-19 10:56:36 +0200

    common layout for login and register

--- src/app/(login-register)/login/page.tsx
+++ src/app/(login-register)/login/page.tsx
@@ -1,13 +1,12 @@
 'use client'
 
 import LoginForm from "@/components/login-register/LoginForm";
-import styles from './page.module.css'
 
 const Page = () => {
     return (
-        <div className={styles.login}>
+        <>
             <LoginForm/>
-        </div>
+        </>
     );
 };
 
---
+++ src/app/(login-register)/register/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import RegisterForm from "@/components/login-register/RegisterForm";
+
+const Page = () => {
+  return (
+    <>
+      <RegisterForm/>
+    </>
+  );
+};
+
+export default Page;
