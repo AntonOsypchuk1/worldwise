772897de0031e978dd09f57c752838419c576968
Антон Осипчук <ipz213_oao@student.ztu.edu.ua>
2024-01-08 16:20:10 +0200

    restyling country list

---
+++ src/components/country/country-info/CountryInfo.tsx
@@ -0,0 +1,7 @@
+import styles from "./CountryInfo.module.css";
+
+const CountryInfo = () => {
+  return <ul className={styles.countryInfo}></ul>;
+};
+
+export default CountryInfo;
--- src/components/country/country-item/CountryItem.tsx
+++ src/components/country/country-item/CountryItem.tsx
@@ -1,15 +1,30 @@
 import styles from "./CountryItem.module.css";
 import { INewCountry } from "@/types/country.interface";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import SmallButton from "@/components/ui/button-small/SmallButton";
 import { HiMenuAlt4 } from "react-icons/hi";
+import CountryInfo from "@/components/country/country-info/CountryInfo";
+import Link from "next/link";
 
 const CountryItem: FC<{ country: INewCountry }> = ({ country }) => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <li className={styles.countryItem}>
-      <span className={styles.emoji}>{country.emoji}</span>
-      <span className={styles.name}>{country.country}</span>
-      <SmallButton icon={<HiMenuAlt4 />} type="expand" />
+    <li>
+      <Link
+        href={`/countries?code=${country.countryCode}`}
+        className={styles.countryItem}
+      >
+        <span className={styles.emoji}>{country.emoji}</span>
+        <span className={styles.name}>{country.countryName}</span>
+        <SmallButton
+          onClick={() => setExpanded((value) => !value)}
+          icon={<HiMenuAlt4 />}
+          type="expand"
+        />
+      </Link>
+
+      {expanded && <CountryInfo />}
     </li>
   );
 };
